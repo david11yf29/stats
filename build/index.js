1a877b4a74f994596ce200653329298799bba0bd
david11yf29 <david11yf29@gmail.com>
2021-01-11 00:51:48 +0800

    updated

--- build/index.js
+++ build/index.js
@@ -1,2 +1,34 @@
 "use strict";
-console.log('hi there!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+var manUnitedWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === "A") {
+        manUnitedWins++;
+    }
+}
+console.log(manUnitedWins);
+// [
+//   [start:run]   [
+//   [start:run]     '10/08/2018',
+//   [start:run]     'Man United',
+//   [start:run]     'Leicester',
+//   [start:run]     '2',
+//   [start:run]     '1',
+//   [start:run]     'H',
+//   [start:run]     'A Marriner'
+//   // [start:run]   ],
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('hi there!')
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+})
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',')
+  })
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === "A") {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
+
+// [
+//   [start:run]   [
+//   [start:run]     '10/08/2018',
+//   [start:run]     'Man United',
+//   [start:run]     'Leicester',
+//   [start:run]     '2',
+//   [start:run]     '1',
+//   [start:run]     'H',
+//   [start:run]     'A Marriner'
+//   // [start:run]   ],
+
