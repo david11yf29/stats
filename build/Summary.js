06cabea37de4e335042920288cff77aab7217b94
david11yf29 <david11yf29@gmail.com>
2021-01-12 20:12:28 +0800

    updated

--- build/Summary.js
+++ build/Summary.js
@@ -6,6 +6,10 @@ var Summary = /** @class */ (function () {
         this.analyzer = analyzer;
         this.outputTarget = outputTarget;
     }
+    ;
+    Summary.prototype.buildAndPrintReport = function (matches) {
+        var output = this.analyzer.run(matches);
+    };
     return Summary;
 }());
 exports.Summary = Summary;
--- src/Summary.ts
+++ src/Summary.ts
@@ -9,7 +9,11 @@ export interface OutputTarget {
 }
 
 export class Summary {
-  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget){}
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget){};
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+  }
 }
 
 new Summary(new WinsAnalysis(), new ConsoleReport());
